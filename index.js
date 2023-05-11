4229d67f6367814c1e33e9e33fb759f686630407
Juan Hidalgo <juan.hidalgo@uao.edu.co>
2023-05-11 03:16:32 -0500

    Http Response

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
-/*
-//Con http
+const express = require("express");
 
-const http = require('http')
-const fs = require('fs')
+const app = express();
 
-const server = http.createServer((req, res) => {
-    const read = fs.createReadStream('./static/index.html')
-    read.pipe(res)
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-server.listen(3000)
-console.log(`server on port ${3000}`)
-*/
-const express = require('express')
+app.get("/miarchivo", (req, res) => {
+  res.sendFile("./flyer.jpeg", {
+    root: __dirname,
+  });
+});
 
-const app = express()
- //operaciones CRUD
- app.get('/products', (req, res) => {
-    res.send('lista de productos')
- })
+app.get("/user", (req, res) => {
+  res.json({ nombre: "Juan", apellido: "Hidalgo", edad: 20, id: 2205622 });
+});
 
- app.post('/products', (req, res) => {
-    res.send('Creando productos')
- })
+app.get("/isAlive", (req, res) => {
+  res.sendStatus(204);
+});
 
- app.put('/products', (req, res) => {
-    res.send('Actualizando productos')
- })
-
- app.patch('/products', (req, res) => {
-    res.send('Actualizando un producto')
- })
-
- app.delete('/products', (req, res) => {
-    res.send('Eliminando productos')
- })
-app.listen(3000)
-console.log(`server on port ${3000}`)
+app.listen(3000);
+console.log(`server on port ${3000}`);
