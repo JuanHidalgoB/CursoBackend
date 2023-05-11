3bb2c0bd4ef862d39cffb726a2de8ebcfd47689d
Juan Hidalgo <juan.hidalgo@uao.edu.co>
2023-05-11 16:59:36 -0500

    haciendo request con la extension Thunder Client (json, forms)

--- index.js
+++ index.js
@@ -2,23 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
-app.get("/miarchivo", (req, res) => {
-  res.sendFile("./flyer.jpeg", {
-    root: __dirname,
-  });
-});
+app.use(express.text()) //para que el servidor pueda leer text
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))//para leer formularios simples, no tiene que interoretar nada complejo
 
-app.get("/user", (req, res) => {
-  res.json({ nombre: "Juan", apellido: "Hidalgo", edad: 20, id: 2205622 });
-});
+app.post("/user", (req, res) => {
+  console.log(req.body);
 
-app.get("/isAlive", (req, res) => {
-  res.sendStatus(204);
+  res.send("Nuevo Usuario Creado");
 });
-
 app.listen(3000);
 console.log(`server on port ${3000}`);
