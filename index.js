27269f4d41e178dc025769612775aa478e299747
Juan Hidalgo <juan.hidalgo@uao.edu.co>
2023-05-12 03:02:36 -0500

    Queries (consultas)

--- index.js
+++ index.js
@@ -3,29 +3,22 @@ const express = require("express");
 const app = express();
 
 app.get('/hello/:username',(req, res)=>{
-    console.log(typeof req.params.username)
+    console.log(req.query)
+    console.log(req.query.age)
+
+    console.log(req.query.age ? `la edad es: ${req.query.age}`: 'No hay edad')
     res.send(`Hello ${req.params.username.toUpperCase()}`)
 })
 
-app.get('/add/:x/:y', (req, res)=>{
-
-    //se puede hacer lo siguiente para sacar los parametros más fácil
-    //const {x,y} = req.params 
-
-    const suma = parseInt(req.params.x) + parseInt(req.params.y)
-    res.send(`${req.params.x} + ${req.params.y} = ${suma}`)
-
-})
-
-app.get('/:user/photo', (req, res) => {
-    const {user} = req.params
-    if (user === 'juan'){
-        return res.sendFile('./flyer.jpeg', {
-            root: __dirname
-        })
+app.get('/search',(req, res) => {
+    const {color, points} = req.query
+    if (color === 'blue' && points>=10){
+        res.send('el color es azul')
+    }else{
+        res.send('No hay nada')
     }
-    res.send('Usuario sin acceso')
-    
 })
+
+
 app.listen(3000);
 console.log(`server on port ${3000}`);
