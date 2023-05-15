a920ec262ad526801f2fc908e260539f7240c17f
Juan Hidalgo <juan.hidalgo@uao.edu.co>
2023-05-15 00:52:34 -0500

    Middlewares pasó por aquí

--- index.js
+++ index.js
@@ -2,23 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get('/hello/:username',(req, res)=>{
-    console.log(req.query)
-    console.log(req.query.age)
+app.use((req, res, next) => {
+    console.log(`Route: ${req.url} Method: ${req.method}`)
 
-    console.log(req.query.age ? `la edad es: ${req.query.age}`: 'No hay edad')
-    res.send(`Hello ${req.params.username.toUpperCase()}`)
+    next()
 })
 
-app.get('/search',(req, res) => {
-    const {color, points} = req.query
-    if (color === 'blue' && points>=10){
-        res.send('el color es azul')
-    }else{
-        res.send('No hay nada')
-    }
+app.get('/profile', (req, res) => {
+    res.send('Profile Page')
 })
 
+//esto de abajo lo probé yo
+app.use((req, res) => {
 
+    res.send(`No existe tu ruta /${req.url} `)
+})
 app.listen(3000);
 console.log(`server on port ${3000}`);
