6dcee49fc0001a1a900ded01d63aa1d1393d1776
Juan Hidalgo <juan.hidalgo@uao.edu.co>
2023-05-25 02:39:02 -0500

    Archivos Staticos, y configuracion cuando guardamos el index en y otras carpetas en otra carpeta por ejemplo (src)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const morgan = require('morgan')
-
+const path = require('path')
 
 const app = express();
 
 app.use(morgan('tiny'))
 
+
+
 app.get('/profile', (req, res) => {
     res.send('Profile Page')
+})
+
+app.get('/note.txt', (req,res) => { //para acceder a esta ruta y no se confunda con el archivo de static
+    res.send('esto no es un archivo')
 })
 
 app.get('/about', (req, res) => {
@@ -15,13 +21,13 @@ app.get('/about', (req, res) => {
 })
 
 //a este middleware le puedo poner la ruta de /dashboard para que solo se aplique cuando quiera ir a esa ruta (creo que es la manera correcta)
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     if (req.query.login === 'juan@uao.edu.co'){
         next()
     }else{
         res.send('No autorizado')
     }
-})
+})*/
 
 app.get('/dashboard', (req, res) => {
     res.sendFile('./static/index.html',{
@@ -29,11 +35,15 @@ app.get('/dashboard', (req, res) => {
     })
 })
 
-
-
+//app.use(express.static('./static'))
+app.use('/public',express.static(path.join(__dirname,'static'))) //para acceder a los archivos de static cuando lo metemos en una carpeta
 //esto de abajo lo probé yo
 app.use((req, res) => {
     res.send(`No existe tu ruta /${req.url} `)
 })
+
+
+
+
 app.listen(3000);
 console.log(`server on port ${3000}`);
